6bebdcd714e93205a60816092e111219dff76d9c
bawumia23 <bawumia91@gmail.com>
2026-09-07 02:30:59 +0000

    Add do_create command to console

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines the HBNB console (command interpreter)."""
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -20,6 +21,18 @@ class HBNBCommand(cmd.Cmd):
         """EOF signal to exit the program."""
         print()
         return True
+
+    def do_create(self, arg):
+        """Create a new instance of a class, save it, print its id."""
+        if not arg:
+            print("** class name missing **")
+            return
+        if arg != "BaseModel":
+            print("** class doesn't exist **")
+            return
+        new_instance = BaseModel()
+        new_instance.save()
+        print(new_instance.id)
 
 
 if __name__ == "__main__":
