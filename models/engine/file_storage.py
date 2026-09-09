3fdc1e18b025d49720cf86ac6c478b19b3364e45
bawumia23 <bawumia91@gmail.com>
2026-09-09 14:44:10 +0000

    Refactor reload method to check class type

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Defines the FileStorage class."""
 import json
-from models.base_model import BaseModel
 
 
 class FileStorage:
@@ -28,12 +27,15 @@ class FileStorage:
 
     def reload(self):
         """Deserializes the JSON file to __objects if file exists."""
+        from models.base_model import BaseModel
+        
         try:
             with open(FileStorage.__file_path, "r") as f:
                 objdict = json.load(f)
                 for o in objdict.values():
                     cls_name = o["__class__"]
                     del o["__class__"]
-                    self.new(BaseModel(**o))
+                    if cls_name == "BaseModel":
+                        self.new(BaseModel(**o))
         except FileNotFoundError:
             return
